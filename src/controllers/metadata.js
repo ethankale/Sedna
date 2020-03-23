2fd63172e14ca22425881db2f27e0c6b6d8a2244
ethankale <nathaniel.kale@gmail.com>
2020-03-23 11:10:41 -0700

    created a form in the metadata page.  Still need to load data into it.

--- src/controllers/metadata.js
+++ src/controllers/metadata.js
@@ -12,16 +12,23 @@ let controller = {
         
         var active    = req.query.active;
         
-        var statement = `SELECT md.MetadataID, st.Code + ': ' + st.Name + ' (' + sp.Name + ')' as Metaname
-            FROM Metadata as md
-            INNER JOIN SamplePoint as sp
-            ON md.SamplePointID = sp.SamplePointID
-            INNER JOIN Site as st
-            ON sp.SiteID = st.SiteID`
-            
+        var statement = `SELECT md.MetadataID, st.Code + ': ' + st.Name + 
+          ' (' + pm.Name + ' | ' + mt.Code + ' | ' + sp.Name + ')' as Metaname
+          FROM Metadata as md
+          INNER JOIN SamplePoint as sp
+          ON md.SamplePointID = sp.SamplePointID
+          INNER JOIN Site as st
+          ON sp.SiteID = st.SiteID
+          INNER JOIN Parameter as pm
+          ON pm.ParameterID = md.ParameterID
+          INNER JOIN Method as mt
+          ON mt.MethodID = md.MethodID\r`
+          
         if (typeof active != "undefined") {
-            statement = statement + " AND Active <> 0"
+          statement = statement + " WHERE Active <> 0\r"
         }
+        
+        statement = statement + " ORDER BY st.Code"
         
         console.log(statement);
         
@@ -37,9 +44,18 @@ let controller = {
     getMetadataDetails: function (req, res) {
         var connection = new Connection(mssql_config);
         
-        var statement = `SELECT SiteID, ParameterID, MethodID, Name, Method, maxdtm, mindtm, nmeasure
-          FROM [Measurement_By_SamplePoint_v]
-          WHERE SiteID = ${req.query.siteid}
+        var metaid = req.query.metadataid;
+        
+        var statement = `SELECT [MetadataID]
+          ,[ParameterID]
+          ,[UnitID]
+          ,[SamplePointID]
+          ,[Notes]
+          ,[MethodID]
+          ,[Active]
+          ,[FrequencyMinutes]
+          ,[DecimalPoints]
+          WHERE MetadataID = ${metaid}
           ORDER BY Name`
         
         connection.on('connect', function(err) {
