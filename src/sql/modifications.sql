f049d32516f4698cfcfd60a8fc3d269e7c36e71c
ethankale <nathaniel.kale@gmail.com>
2020-03-27 16:12:40 -0700

    added a bunch of columns to sample points table in anticipation of creating CRUD for this next week.  should now have everything from the original GData table.

--- src/sql/modifications.sql
+++ src/sql/modifications.sql
@@ -12,3 +12,25 @@ SET Active = 1
 
 ALTER TABLE Site
 ALTER COLUMN Active bit NOT NULL
+
+ALTER TABLE SamplePoint
+ADD LatLongAccuracyFeet smallint NULL,
+LatLongDate datetime2(0) NULL,
+LatLongDetails varchar(255) NULL,
+ElevationAccuracyFeet smallint NULL,
+ElevationDate datetime2(0) NULL,
+ElevationDetails varchar(255) NULL,
+WellType varchar(100) NULL,
+WellCompletionType varchar(100) NULL,
+WellIntervalTopFeet numeric(6,2) NULL,
+WellIntervalBottomFeet numeric(6,2) NULL,
+WellInnerDiameterInches numeric(4,2) NULL,
+WellOuterDiameterInches numeric(4,2) NULL,
+WellStickupFeet numeric(4,2) NULL,
+WellStickupDate datetime2(0) NULL,
+WellDrilledBy varchar(100) NULL,
+WellEcologyTagID varchar(10) NULL,
+WellEcologyStartCardID varchar(15),
+AddedOn datetime2(0) NULL,
+RemovedOn datetime2(0) NULL,
+Active bit DEFAULT 1 NOT NULL
