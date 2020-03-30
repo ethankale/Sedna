dbd10e9fbc05c92c7a44f50f09146b9a72f7c6aa
ethankale <nathaniel.kale@gmail.com>
2020-03-30 15:18:48 -0700

    select2 and vuejs are playing nicely now.  at long last.

--- src/js/metadata_bundle.js
+++ src/js/metadata_bundle.js
@@ -12246,17 +12246,36 @@ if (process.env.NODE_ENV === 'production') {
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("timers").setImmediate)
 },{"timers":2}],6:[function(require,module,exports){
 
-var Vue = require('vue')
+let Vue = require('vue')
+//let vSelect = require('vue-select');
+
+Vue.directive('select', {
+  twoWay: true,
+  bind: function (el, binding, vnode) {
+    $(el).select2().on("select2:select", (e) => {
+      // v-model looks for
+      //  - an event named "change"
+      //  - a value with property path "$event.target.value"
+      el.dispatchEvent(new Event('change', { target: e.target }));
+    });
+  },
+});
 
 $(document).ready(function() {
   $("#spSelect").select2({ width: '100%' });
 });
 
+//Vue.component('v-select', vSelect)
+
 var vm = new Vue({
   el: '#v-pills-sample-point',
   data: {
     notice: 'Vue is working!',
-    samplePointID: -1,
+    spID: 2,
+    currentSP: {
+      SamplePointID: 2, 
+      Name: 'Loading...'
+    },
     sps: []
   },
   mounted: function () {
@@ -12267,11 +12286,27 @@ var vm = new Vue({
       timeout: 3000
     }).done(function(data) {
       self.sps = data;
-      self.samplePointID = data[0].SamplePointID;
+      self.getCurrentSP(data[0].SamplePointID);
     }).fail(function(err) {
       console.log(err);
     });
+  },
+  methods: {
+    getCurrentSP: function(SamplePointID) {
+      let self = this;
+      $.ajax({
+        url: `http://localhost:3000/api/v1/samplePoint?samplepointid=${SamplePointID}`,
+        method:'GET',
+        timeout: 3000
+      }).done(function(data) {
+        self.currentSP = data;
+        console.log(self.currentSP);
+      }).fail(function(err) {
+        console.log(err);
+      });
+    }
   }
+    
 })
 
 
